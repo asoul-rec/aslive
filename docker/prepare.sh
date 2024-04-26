1aaff8a6d94f2cadd501cbe1430a511317f5b97e
asoul-rec <asoulranbi@gmail.com>
2024-04-26 04:27:43 -0400

    update docker building config: avoid incompatibility between cython3.0 and pyav compile tgcrypto in requirement-stage enable http protocol in ffmpeg compilation

--- docker/prepare.sh
+++ docker/prepare.sh
@@ -2,6 +2,6 @@ mkdir -p /root/files/lib/python3.11/site-packages/
 mkdir -p /root/files/bin/
 cp /usr/local/bin/ffmpeg /root/files/bin/
 cd /usr/local/lib
-cp -a python3.11/site-packages/av* /root/files/lib/python3.11/site-packages/
 cp -a libavcodec* libavdevice* libavfilter* libavformat* libavutil* libswresample* libswscale* /root/files/lib
-
+cd python3.11/site-packages
+cp -a av* tgcrypto* TgCrypto* /root/files/lib/python3.11/site-packages/
