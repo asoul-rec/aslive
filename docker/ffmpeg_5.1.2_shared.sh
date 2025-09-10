37df55f634ff74eda3993fe7f76f3ec2666eb269
asoul-rec <asoulranbi@gmail.com>
2025-09-10 02:42:05 -0400

    Update docker recipe

--- docker/ffmpeg_5.1.2_shared.sh
+++
@@ -1,7 +0,0 @@
-apk add --no-cache build-base openssl-dev nasm x264-dev
-wget https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.xz -O ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz
-cd ffmpeg-5.1.2
-./configure --disable-w32threads --disable-os2threads --disable-alsa --disable-appkit --disable-avfoundation --disable-bzlib --disable-coreimage --disable-iconv --disable-libxcb --disable-libxcb-shm --disable-libxcb-xfixes --disable-libxcb-shape --disable-lzma --disable-sndio --disable-sdl2 --disable-xlib --disable-zlib --disable-amf --disable-audiotoolbox --disable-cuda --disable-cuvid --disable-d3d11va --disable-dxva2 --disable-nvdec --disable-nvenc --disable-v4l2-m2m --disable-vaapi --disable-vdpau --disable-videotoolbox --disable-everything --enable-demuxer=flv,mov,mpegts,live_flv --enable-muxer=flv,mov,mp4,mpegts --enable-protocol=file,rtmp,pipe,rtmps,http --enable-bsf=aac_adtstoasc,h264_mp4toannexb --enable-encoder=aac,h264 --enable-decoder=aac,h264 --disable-postproc --disable-doc --disable-runtime-cpudetect --enable-openssl --enable-pthreads --enable-version3 --enable-gpl --enable-libx264 --enable-avcodec --enable-avformat --enable-avdevice --enable-swscale --enable-swresample --enable-avfilter --disable-programs --enable-ffmpeg --enable-small --enable-shared --disable-static
-make -j4
-make install
--- docker/prepare.sh
+++ docker/prepare.sh
@@ -1,7 +1,7 @@
-mkdir -p /root/files/lib/python3.11/site-packages/
+mkdir -p /root/files/lib/python3.13/site-packages/
 mkdir -p /root/files/bin/
-cp /usr/local/bin/ffmpeg /root/files/bin/
+cp /usr/local/bin/ffmpeg /usr/local/bin/ffprobe /root/files/bin/
 cd /usr/local/lib
 cp -a libavcodec* libavdevice* libavfilter* libavformat* libavutil* libswresample* libswscale* /root/files/lib
-cd python3.11/site-packages
-cp -a av* tgcrypto* TgCrypto* /root/files/lib/python3.11/site-packages/
+cd python3.13/site-packages
+cp -a av* /root/files/lib/python3.13/site-packages/
